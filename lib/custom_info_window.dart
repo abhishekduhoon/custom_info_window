524235361481d58fc80843ed5d98a12b4752426e
usmanokara <usman.okara@hotmail.com>
2022-09-19 05:18:17 +0500

    plugin update (#13)

--- lib/custom_info_window.dart
+++ lib/custom_info_window.dart
@@ -46,7 +46,10 @@ class CustomInfoWindow extends StatefulWidget {
   /// Width of [CustomInfoWindow].
   final double width;
 
-  const CustomInfoWindow({
+  final Function(double top, double left, double width, double height) onChange;
+
+  const CustomInfoWindow(
+    this.onChange, {
     required this.controller,
     this.offset = 50,
     this.height = 50,
@@ -100,6 +103,7 @@ class _CustomInfoWindowState extends State<CustomInfoWindow> {
       _leftMargin = left;
       _topMargin = top;
     });
+    widget.onChange.call(top, left, widget.width, widget.height);
   }
 
   /// Assign the [Widget] and [Marker]'s [LatLng].
